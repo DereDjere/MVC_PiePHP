9cb7c652b9ff7e6408fb744ae6398436770da5fe
Idriss <idriss-yasine.zaouche@epitech.eu>
2020-03-25 19:40:15 +0100

    Structure MVC - CORE START

---
+++ src/View/User/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<pre>
+<?php
+var_dump($_POST);
+    echo $_POST['email'];
+?>
+</pre>
+<pre>
+<?php
+var_dump($_GET);
+    echo $_GET;
+?>
+</pre>
+<pre>
+<?php
+    echo $_SERVER;
+?>
+</pre>
+</body>
+</html>
---
+++ src/View/User/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="/SC_Project/Project/MVC_PiePHP/index.php" method="post">
+        <label for="email">Enter your email:</label>
+        <input type="email" id="email" name="email">
+        <label for="pwd">Password:</label>
+        <input type="password" id="pwd" name="pwd">
+        <div class="form-example">
+            <input type="submit" value="Subscribe!">
+        </div>
+    </form>
+</body>
+
+</html>
